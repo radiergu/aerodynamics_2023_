1af2021d6e5cde504dffc583bc200b573015ae33
radie <radie@juju_lenovo>
2023-12-03 00:52:58 +0100

    added comparison on thin airfoil

--- thin_airfoil_theory.m
+++ thin_airfoil_theory.m
@@ -2,6 +2,23 @@ clc;
 clear;
 close all;
 
+%% Open some datas
+
+[numbers, ~] = xlsread("Extracted data.xlsx", 'Feuille 1'); %#ok<XLSRD>
+if ~isempty(numbers)
+    data =  numbers;
+end
+
+x_3per = rmmissing(data(:,6));
+y_3per = rmmissing(data(:,7));
+
+x_9per = rmmissing(data(:,8));
+y_9per = rmmissing(data(:,9));
+
+% list 
+alpha_paper = {x_3per, x_9per};
+cl_paper    = {y_3per, y_9per};
+
 %% generate airfoil
 
 c = 1; % Chord length
@@ -25,25 +42,30 @@ plot(alpha, cl, 'b')
 
 %% checking severall camb
 
+tc_ = [3, 9]./100;
 
-tc_ = floor(linspace(0,9,3))./100;
-
-figure; hold on; grid on;
-legend = [];
+figure(2); hold on; grid on;
+legend_ = [""];
 
 A0 = deg2rad(alpha);
 
-for k = 1:3
+l = 1;
+
+for k = 1:2
     
     A1(1:length(A0)) = 4*tc_(k);
 
     cl = pi.*(2 * A0 + A1);
 
     plot(alpha, cl)
-    legend(k) = "tc = " + string(tc);
-    
-end
+    plot(alpha_paper{k}, cl_paper{k})
 
-legend(legend);
+    legend_(l)     = "tc = " + string(tc);
+    legend_(l + 1) = "Paper, tc = " + string(tc);
+    l = l + 2;
+end
+    
+    figure(2);
+    legend(legend_);
 
 
