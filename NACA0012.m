72b66b0eea61e40879503c9646bc418e39d53f2c
TimourJ <timour.jestin@epfl.ch>
2023-12-12 23:36:26 +0100

    Updates

--- NACA0012.m
+++ NACA0012.m
@@ -2,6 +2,9 @@ clc;
 clear;
 close all;
 
+set(0, 'defaultAxesTickLabelInterpreter','latex');
+set(0, 'defaultLegendInterpreter','latex');
+set(0,'defaultAxesFontSize',20)
 set(0,'DefaultLineLineWidth',1.5);
 %% Airfoil Characteristics
 
@@ -15,7 +18,9 @@ a = (c./4) * ( 1 + 2.*tc.*4/(3.*sqrt(3)) )  .* (1 + tc.*4/(3.*sqrt(3)) )^(-2);
 b = a .* tc .* 4/(3.*sqrt(3));
 
 R = a + b;
-Zeta_origin = -b;
+Zeta_origin = -b; % forward facing
+Zeta_origin = b; % backward facing
+
 Zeta_circ = R*exp(1i*linspace(0,2*pi,200)) + Zeta_origin;
 
 
@@ -38,7 +43,7 @@ grid on, grid minor
 xlabel('x'), ylabel('y')
 
 %% Flow Field
-alpha = 0; % AoA in degrees
+alpha = 5; % AoA in degrees
 alpha = deg2rad(alpha);
 
 Re = 2e4;
@@ -50,33 +55,63 @@ Gamma = -4 .* pi .* R .* uinf .* sin(alpha); % alpha
 n = 400;
 bound = 3*R;
 [eta, xi] = meshgrid(linspace(-bound,bound,n), linspace(bound,-bound,n));
-
 zeta = eta + 1i .* xi;
-
+cond = abs(zeta-Zeta_origin)<=R;
+zeta(cond) = (R).*exp(-1i.* linspace(0,2*pi,length(zeta(cond))) ) + Zeta_origin;
+eta = real(zeta);
+xi = imag(zeta);
+x = real(joukowski_mapping(zeta,a));
+y = imag(joukowski_mapping(zeta,a));
 
 w = flow_field_gamma(alpha, uinf, R, Gamma, zeta, Zeta_origin);
 
 psi = imag(w);
 
+U_zeta = velocity_field_gamma(alpha, uinf, R, Gamma, zeta, Zeta_origin);
+% u_zeta = real(U_zeta);
+% v_zeta = -imag(U_zeta);
+
+U_real = U_zeta ./(1 - a.^2./(zeta.^2) );
+% u_real = real(U_real);
+% v_real = -imag(U_real);
+
+
+
 nlevels = 6;
 
-levels = imag(flow_field_gamma(alpha, uinf, R, Gamma, -bound + 1i.*linspace(0,bound,nlevels), Zeta_origin));
+levels = imag(flow_field_gamma(alpha, uinf, R, Gamma, (-10*R + 1i.*linspace(0,2*R,nlevels)).*exp(1i.*alpha) , Zeta_origin));
 
 if ~ismember(0,levels)
     levels = cat(2,0,levels);
 end
 
-levels = sort(cat(2,-levels(2:end),levels));
+levels = sort(cat(2,-levels(2:end),levels))
 
 figure
 hold on
+colormap hot;
+h1 = pcolor(eta,xi,abs(U_zeta));
 contour(eta,xi,psi,levels,'k-')
 fill(real(Zeta_circ),imag(Zeta_circ),'b','LineStyle','none')
-plot(complex(Zeta_origin),'kx')
 hold off
 axis square
 grid on, grid minor
+set(h1, 'EdgeColor', 'none');
+colorbar
 savefig('FlowContourJouk.fig')
+
+
+figure
+hold on
+colormap hot;
+h2 = pcolor(x,y,abs(U_real));
+contour(x,y,psi,levels,'k-')
+%fill(real(joukowski_mapping(Zeta_circ,a)),imag(joukowski_mapping(Zeta_circ,a)),'b','LineStyle','none')
+hold off
+axis square
+grid on, grid minor
+set(h2, 'EdgeColor', 'none');
+colorbar
 
 %%
 narrows = nlevels*4;
@@ -110,7 +145,6 @@ min(y2,[],"all")
 zflow = linspace(min(x2,[],"all"),0,5) .* exp(1i.*alpha);
 
 
-
 figure
 hold on
 fill(real(Zeta_circ),imag(Zeta_circ),'b','LineStyle','none')
@@ -120,14 +154,13 @@ axis square
 grid on, grid minor
 savefig('FlowArrowsJouk.fig')
 
-cond = abs(zeta-Zeta_origin)<=R;
-zeta(cond) = R.*exp(-1i.* linspace(0,2*pi,length(zeta(cond))) ) + Zeta_origin;
-
-figure
-colormap hot;
-h = pcolor(real(joukowski_mapping(zeta,a)),imag(joukowski_mapping(zeta,a)),abs( velocity_field_gamma(alpha, uinf, R, Gamma, zeta, Zeta_origin) ./(1 - a.^2./(zeta.^2) ) ));
-set(h, 'EdgeColor', 'none');
-colorbar
+% cond = abs(zeta-Zeta_origin)<=R;
+% zeta(cond) = R.*exp(-1i.* linspace(0,2*pi,length(zeta(cond))) ) + Zeta_origin;
+% figure
+% colormap hot;
+% h = pcolor(real(joukowski_mapping(zeta,a)),imag(joukowski_mapping(zeta,a)),abs( velocity_field_gamma(alpha, uinf, R, Gamma, zeta, Zeta_origin) ./(1 - a.^2./(zeta.^2) ) ));
+% set(h, 'EdgeColor', 'none');
+% colorbar
 
 figure
 hold on
@@ -135,7 +168,7 @@ plot(complex(zflow),"k--")
 fill(real(joukowski_mapping(Zeta_circ,a)),imag(joukowski_mapping(Zeta_circ,a)),'b','LineStyle','none')
 quiver(x2,y2,u_real,v_real,'k')
 hold off
-legend({"","freestream flow", "", ""},'Location','best')
+legend({"freestream flow", "", ""},'Location','best')
 axis square
 grid on, grid minor
 
